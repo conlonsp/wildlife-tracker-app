9574c549cd774004206cf3eacc6c1a6ca833b63b
samuelconlon <conlonsp@yahoo.com>
2023-06-15 09:07:22 -0400

    add validations to organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,7 @@ class Organization < ApplicationRecord
   has_many :user_organizations
   has_many :users, through: :user_organizations
   has_many :projects
+
+  validates :name, :mission, presence: true
+  validates :mission, length: { in: 50..250 }
 end
