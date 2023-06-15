776a8f9f4debae6d5ea8376effd60e8ac0ce6c95
samuelconlon <conlonsp@yahoo.com>
2023-06-15 09:38:40 -0400

    create seed data for users and user_organizations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,182 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+## Users ##
+
+user1 = User.create({
+  username: "noot",
+  password: "woofwoof",
+  password_confirmation: "woofwoof",
+  bio: "I'm a dog and I like balls and swimming.",
+  avatar_url: "https://cache.lovethispic.com/uploaded_images/blogs/10-Beautiful-Nature-Scenes-And-Landscapes-48771-6.jpeg"
+})
+
+user2 = User.create({
+  username: "sam",
+  password: "rawknroll",
+  password_confirmation: "rawknroll",
+  bio: "I'm a human and I want to rawk.",
+  avatar_url: "https://cache.lovethispic.com/uploaded_images/blogs/10-Beautiful-Nature-Scenes-And-Landscapes-48771-9.jpeg"
+})
+
+## User_Organizations ##
+
+user_org1 = User_organization.create({
+  user_id: 1,
+  organization_id: 1
+})
+
+user_org2 = User_organization.create({
+  user_id: 1,
+  organization_id: 2
+})
+
+user_org3 = User_organization.create({
+  user_id: 2,
+  organization_id: 3
+})
+
+user_org4 = User_organization.create({
+  user_id: 2,
+  organization_id: 4
+})
+
+## Organizations ##
+
+org1 = Organization.create({
+  name: "",
+  mission: "",
+  location: ""
+})
+
+org2 = Organization.create({
+  name: "",
+  mission: "",
+  location: ""
+})
+
+org3 = Organization.create({
+  name: "",
+  mission: "",
+  location: ""
+})
+
+org4 = Organization.create({
+  name: "",
+  mission: "",
+  location: ""
+})
+
+org5 = Organization.create({
+  name: "",
+  mission: "",
+  location: ""
+})
+
+org6 = Organization.create({
+  name: "",
+  mission: "",
+  location: ""
+})
+
+org7 = Organization.create({
+  name: "",
+  mission: "",
+  location: ""
+})
+
+org8 = Organization.create({
+  name: "",
+  mission: "",
+  location: ""
+})
+
+## Projects ##
+
+proj1 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj2 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj3 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj4 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj5 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj6 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj7 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj8 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj9 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
+proj10 = Project.create({
+  name: "",
+  description: "",
+  location: "",
+  start_date: "",
+  end_date: "",
+  organization_id: 
+})
+
