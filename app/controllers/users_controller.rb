f81b714ced5d90057adc4a6fd7cb9110c1079607
samuelconlon <conlonsp@yahoo.com>
2023-06-15 21:24:26 -0400

    write create action for signup functionality in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
 end
