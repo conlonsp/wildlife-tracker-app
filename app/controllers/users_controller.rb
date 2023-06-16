af150ab799f079b165599dc6ae349c4ad7812789
samuelconlon <conlonsp@yahoo.com>
2023-06-15 21:26:16 -0400

    write private user_params helper method for create action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
     session[:user_id] = user.id
     render json: user, status: :created
   end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation, :avatar_url, :bio)
+  end
 end
