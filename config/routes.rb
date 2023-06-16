956fadc146ce3b66e1512ec366275bb6eb5220be
samuelconlon <conlonsp@yahoo.com>
2023-06-16 18:36:11 -0400

    add routes for create and show users_controller methods in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  
+  post '/login', to 'users#create'
+  get '/me', to 'users#show'
+
   resources :donations
   resources :user_organizations
   resources :projects
   resources :organizations
-  resources :users
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
