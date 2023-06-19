854ee81280f62204dc263f241474efc06c1faefc
samuelconlon <conlonsp@yahoo.com>
2023-06-19 15:14:42 -0400

    pass setUser setter function from LoginSignup to Login and Signup forms

--- client/src/pages/LoginSignup.js
+++ client/src/pages/LoginSignup.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react'
 import LoginForm from '../components/LoginForm'
 import SignupForm from '../components/SignupForm'
 
-function LoginSignup() {
+function LoginSignup({ setUser }) {
   const [toLogin, setToLogin] = useState(false)
 
   return (
     <div>
       {toLogin ?
-        <LoginForm setToLogin={setToLogin}/>
+        <LoginForm setToLogin={setToLogin} setUser={setUser} />
       :
-        <SignupForm setToLogin={setToLogin}/>
+        <SignupForm setToLogin={setToLogin} setUser={setUser} />
       }
     </div>
   )
