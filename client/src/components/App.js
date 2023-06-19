c10360536f7536269cc784068ec92bfdf037bbd6
samuelconlon <conlonsp@yahoo.com>
2023-06-19 12:15:04 -0400

    import LoginSignup component in App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import logo from '../logo.svg';
 import '../App.css';
+import LoginSignup from '../pages/LoginSignup';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <LoginSignup />
     </div>
   );
 }
