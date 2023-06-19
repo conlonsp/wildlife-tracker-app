7fa25301d432651b9f8a964a03780f3b88cc6b52
samuelconlon <conlonsp@yahoo.com>
2023-06-19 15:05:06 -0400

    create user state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,8 @@ import '../App.css';
 import LoginSignup from '../pages/LoginSignup';
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <div className="App">
       {/* <header className="App-header">
