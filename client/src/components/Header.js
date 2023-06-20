fe141720fb144d157c37fc28917d55ad9c735ef0
samuelconlon <conlonsp@yahoo.com>
2023-06-20 16:12:53 -0400

    write handleLogout function in Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,14 @@ import { UserContext } from "../Context";
 
 function Header() {
   const { user, setUser } = useContext(UserContext)
+
+  function handleLogout() {
+    fetch('/logout', { method: "DELETE" }).then(r => {
+      if(r.ok) {
+        setUser(null)
+      }
+    })
+  }
   
   return (
     <div>
