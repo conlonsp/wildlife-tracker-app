76e9e9f6f665acc4a7ea4b4e83c33fadd2adb647
samuelconlon <conlonsp@yahoo.com>
2023-06-20 15:06:57 -0400

    useContext to pass user and setUser to Dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { UserContext } from '../Context'
 
 function Dashboard() {
+  const { user, setUser } = useContext(UserContext)
 
   return (
     <h1>Dashboard</h1>
