db589b665725dfb084612398ea9d6d56ca98765e
samuelconlon <conlonsp@yahoo.com>
2023-06-20 15:12:33 -0400

    write basic login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,10 +3,23 @@ import { UserContext } from "../Context";
 
 function LoginForm({ setToLogin }) {
   const { setUser } = useContext(UserContext)
-  
+
   return (
     <div>
       <h1>LoginForm</h1>
+      <form>
+        <input
+          type='text'
+          name='username'
+          placeholder="Username"
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder="Password"
+        />
+        <button>Login</button>
+      </form>
       <button onClick={()=>setToLogin(false)}>Signup</button>
     </div>
   )
