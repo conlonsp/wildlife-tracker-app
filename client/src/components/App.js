2c391985478ce028a08d60c79f43b908b22aae0b
samuelconlon <conlonsp@yahoo.com>
2023-06-20 15:41:10 -0400

    import Dashboard in App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../App.css';
 
+import Dashboard from '../pages/Dashboard';
 import LoginSignup from '../pages/LoginSignup';
 
 function App() {
