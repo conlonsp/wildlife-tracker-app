3c229b4cb0041b817aa2473b9dede296560f9209
samuelconlon <conlonsp@yahoo.com>
2023-06-21 09:37:33 -0400

    implement basic NavBar functionality

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,12 @@ function NavBar() {
   return (
     <div>
       <h1>This is the NavBar</h1>
-
+      <NavLink
+        to='/'
+        exact
+      >
+        Dashboard
+      </NavLink>
     </div>
   )
 }
