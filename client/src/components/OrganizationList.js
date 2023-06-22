b93e684b0e5a6bfa04a373c6f0059ffc2aaf6c9d
samuelconlon <conlonsp@yahoo.com>
2023-06-22 13:52:49 -0400

    add useEffect for fetching organizations

--- client/src/components/OrganizationList.js
+++ client/src/components/OrganizationList.js
@@ -2,6 +2,17 @@ import React from "react";
 
 function OrganizationList() {
 
+  useEffect(() => {
+    fetch('/organizations')
+    .then(r => {
+      if(r.ok) {
+        r.json().then(orgs => setOrganizations(orgs))
+      } else {
+        r.json().then(err => setErrors(err.errors))
+      }
+    })
+  }, [])
+
   return (
     <div>
       <h1>Organization List</h1>
