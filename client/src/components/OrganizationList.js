837b2ba04ed1e428bed3a40e6d938310e27969e5
samuelconlon <conlonsp@yahoo.com>
2023-06-22 13:53:52 -0400

    create organizations state

--- client/src/components/OrganizationList.js
+++ client/src/components/OrganizationList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function OrganizationList() {
+  const [organizations, setOrganizations] = useState([])
 
   useEffect(() => {
     fetch('/organizations')
