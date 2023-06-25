49e982ed9e816d5acb1a5d8d6ca7dfd506b0ef71
samuelconlon <conlonsp@yahoo.com>
2023-06-25 18:03:48 -0400

    update rendering of organization list

--- client/src/pages/Organizations.js
+++ client/src/pages/Organizations.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Organizations() {
   const [organizations, setOrganizations] = useState([])
@@ -15,20 +16,27 @@ function Organizations() {
     })
   }, [])
 
-  console.log(organizations)
+  
+
+  const renderOrgs = Object.keys(organizations).map(orgId => (
+    <li key={orgId}>
+      <Link to={`/organizations/${orgId}`}>{organizations[orgId].name}</Link>
+    </li>
+  ))
 
   return (
     <div>
       <h1>Organization List</h1>
-      {organizations.map(org => {
+      <ul>{renderOrgs}</ul>
+      {/* {organizations.map(org => {
         return (
           <li key={org.id}>
             <h3>{org.name}</h3>
-            {/* <h4>{org.mission}</h4>
-            <h5>{org.location}</h5> */}
+            <h4>{org.mission}</h4>
+            <h5>{org.location}</h5>
           </li>
         )
-      })}
+      })} */}
     </div>
   )
 }
