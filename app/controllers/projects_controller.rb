f92b77d2975029540a2a5acf72b4ccb5dad9e262
samuelconlon <conlonsp@yahoo.com>
2023-06-26 16:16:33 -0400

    update index action for nested route in projects_controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,13 @@ class ProjectsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :proj_not_found
 
   def index
-    render json: Project.all, status: :ok
+    if params[:organization_id]
+      org = Organization.find(params[:organization_id])
+      projects = org.projects
+    else
+      projects = Project.all
+    end
+    render json: projects, status: :ok
   end
 
   def show
