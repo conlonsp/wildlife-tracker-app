590299db54acdb97f98f71304b0d995d0e59bbed
samuelconlon <conlonsp@yahoo.com>
2023-06-26 19:16:16 -0400

    create basic form in CreateOrg

--- client/src/components/CreateOrg.js
+++ client/src/components/CreateOrg.js
@@ -5,6 +5,21 @@ function CreateOrg({ setRendCreate }) {
   return (
     <div>
       <h1>Create an Organization here!</h1>
+      <form>
+        <input
+          name='name'
+          placeholder="Organization Name"
+        />
+        <input
+          name='location'
+          placeholder="Based out of..."
+        />
+        <textarea
+          name='mission'
+          placeholder='Our mission is...'
+        />
+        <button>Submit</button>
+      </form>
       <button onClick={() => setRendCreate(false)}>Close</button>
     </div>
   )
