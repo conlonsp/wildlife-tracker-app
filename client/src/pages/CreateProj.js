36d5e352cb406692621f367b7ee0caf084847ea2
samuelconlon <conlonsp@yahoo.com>
2023-06-29 13:57:40 -0400

    add value and onChange event listener to form inputs

--- client/src/pages/CreateProj.js
+++ client/src/pages/CreateProj.js
@@ -29,30 +29,40 @@ function CreateProj({ orgId }) {
           type='text'
           name='name'
           placeholder="i.e. Earth Savers"
+          value={newProj.name}
+          onChange={handleChange}
         />
         <label>Location: </label>
         <input
           type='text'
           name='location'
           placeholder="i.e. New York City, New York, USA"
+          value={newProj.location}
+          onChange={handleChange}
         />
         <label>Start Date: </label>
         <input
           type='text'
           name='start_date'
           placeholder='i.e. YYYY-MM-DD'
+          value={newProj.startDate}
+          onChange={handleChange}
         />
         <label>End Date: </label>
         <input
           type='text'
           name='end_date'
           placeholder='i.e. YYYY-MM-DD'
+          value={newProj.endDate}
+          onChange={handleChange}
         />
         <label>Description: </label>
         <textarea
           type='text'
           name='description'
           placeholder='between 50 - 250 characters'
+          value={newProj.description}
+          onChange={handleChange}
         />
         <button type='submit'>Submit</button>
       </form>
