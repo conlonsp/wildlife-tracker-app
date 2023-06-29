0ce3769d8cc1da11afaf87e4433e3bc6bf03c7e8
samuelconlon <conlonsp@yahoo.com>
2023-06-29 13:01:41 -0400

    create basic project form

--- client/src/pages/CreateProj.js
+++ client/src/pages/CreateProj.js
@@ -8,6 +8,39 @@ function CreateProj({ orgId }) {
   return (
     <div>
       <h1>Project Create Form</h1>
+      <form>
+        <label>Name: </label>
+        <input
+          type='text'
+          name='name'
+          placeholder="i.e. Earth Savers"
+        />
+        <label>Location: </label>
+        <input
+          type='text'
+          name='location'
+          placeholder="i.e. New York City, New York, USA"
+        />
+        <label>Start Date: </label>
+        <input
+          type='text'
+          name='start_date'
+          placeholder='i.e. YYYY-MM-DD'
+        />
+        <label>End Date: </label>
+        <input
+          type='text'
+          name='end_date'
+          placeholder='i.e. YYYY-MM-DD'
+        />
+        <label>Description: </label>
+        <textarea
+          type='text'
+          name='description'
+          placeholder='between 50 - 250 characters'
+        />
+        <button type='submit'>Submit</button>
+      </form>
       <button onClick={() => navigate(`/organizations/${orgId}`)}>Back</button>
     </div>
   )
