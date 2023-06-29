432ca351172f0b2701f826ab39c3abfd2d2b6217
samuelconlon <conlonsp@yahoo.com>
2023-06-29 15:16:56 -0400

    write before_create macro and create_user_organization method in Donation model

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,4 +3,12 @@ class Donation < ApplicationRecord
 
   validates :amount, presence: true
   validates :amount, numericality: { only_integer: true }
+
+  before_create :create_user_organization
+
+  private
+
+  def create_user_organization
+    self.user_organization ||= UserOrganization.create(user_id: self.user.id, organization_id: self.organization.id)
+  end
 end
